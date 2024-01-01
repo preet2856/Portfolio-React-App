27156fe93a189b989966d12873ab6f6341689c6f
preet2856 <preetgadhiya203@gmail.com>
2024-01-01 17:46:47 +0530

    Last Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import './Footer.css'
-import wave from '../../img/wave.png'
-import Insta from '@iconscout/react-unicons/icons/uil-instagram'
-import Face from '@iconscout/react-unicons/icons/uil-facebook'
-import Git from '@iconscout/react-unicons/icons/uil-github'
+import React from 'react';
+import './Footer.css';
+import wave from '../../img/wave.png';
+import Insta from '@iconscout/react-unicons/icons/uil-instagram';
+import Face from '@iconscout/react-unicons/icons/uil-facebook';
+import Git from '@iconscout/react-unicons/icons/uil-github';
 
 const Footer = () => {
-  return (
-    <div className='footer'>
-        <img src={wave} alt="" style={{width: '100%'}}/>
-        <div className="f-content">
-            <span>preetgadhiya@gmail.com</span>
-            <div className="f-icons">
-                <Insta color='white' size='3rem'/>
-                <Face color='white' size='3rem'/>
-                <Git color='white' size='3rem'/>
-            </div>
-        </div>
-    </div>
-  )
+  	return (
+    	<div className='footer'>
+      		<img src={wave} alt="" style={{width: '100%'}}/>
+      		<div className="f-content">
+        		<span>preetgadhiya@gmail.com</span>
+        		<div className="f-icons">
+          			<a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            			<Insta color='white' size='3rem'/>
+          			</a>
+          			<a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            			<Face color='white' size='3rem'/>
+          			</a>
+          			<a href="https://github.com" target="_blank" rel="noopener noreferrer">
+            			<Git color='white' size='3rem'/>
+          			</a>
+        		</div>
+      		</div>
+    	</div>
+  	);
 }
 
-export default Footer
+export default Footer;
+
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,7 +4,7 @@ import HeartEmoji from '../../img/heartemoji.png'
 import Glasses from '../../img/glasses.png'
 import Humble from '../../img/humble.png' 
 import Card from '../Card/Card'
-import Resume from './resume.jpg'
+import Resume from './Preet Gadhiya-FD.pdf'
 
 const Services = () => {
   return (
